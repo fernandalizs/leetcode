07a7c11f704875cfb1f3c49cc3bc059a630a9c59
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-01-11 19:51:22 -0300

    ajusta descrição

--- Python/richest_customer_wealth.py
+++ Python/richest_customer_wealth.py
@@ -1,12 +1,13 @@
 ''' 
-Você recebe uma lista de contas bancárias com a quantidade de dinheiro que os clientes tem no banco. Devolva a riqueza que o cliente mais rico possui.
-A riqueza de um cliente é a quantidade de dinheiro que ele tem em todas as suas contas bancárias. O cliente mais rico é aquele que tem o máximo de riqueza.
+Você recebe uma lista de contas bancárias com a quantidade de dinheiro que os clientes tem no banco.
+Retorne o valor que o cliente mais rico possui.
+A riqueza de um cliente é a quantidade de dinheiro que ele tem em todas as suas contas bancárias somadas.
 
 Exemplo:
 
-Entrada: contas = [[1,2,3],[3,3,1]]
+Entrada: accounts = [[1,2,3],[3,3,1]]
 Saída: 7
-Explicação :
+Explicação:
 1 cliente possui = 1 + 2 + 3 = 6
 2 cliente possui = 3 + 3 + 1 = 7
 O cliente mais rico é o 2 com uma riqueza de 7, então retorne 7.
