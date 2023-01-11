d2d1d5f12295958e9e271fb5f60d8ad2987c3dd2
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-01-11 15:30:16 -0300

    arruma comentáros

--- JavaScript/349intersectionOfTwoArrays.js
+++ JavaScript/349intersectionOfTwoArrays.js
@@ -1,9 +1,11 @@
-// Dado duas listas nums1 e nums2, retorne um array de sua interseção. Cada elemento no resultado deve ser único e você pode retornar o resultado em qualquer ordem.
+/* 
+Dado duas listas nums1 e nums2, retorne um array de sua interseção. Cada elemento no resultado deve ser único e você pode retornar o resultado em qualquer ordem.
 
-// Exemplo 1:
+Exemplo 1:
 
-// Entrada: nums1 = [1,2,2,1], nums2 = [2,2]
-//  Saída: [2]
+Entrada: nums1 = [1,2,2,1], nums2 = [2,2]
+Saída: [2]
+*/
 
 const intersection = function(nums1, nums2) {
     let list1 = new Set(nums1)
