a1045b670cae11ee3464adf7ddce3c6154885ce9
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-01-12 16:28:23 -0300

    add number_good_pairs.py

---
+++ Python/number_good_pairs.py
@@ -0,0 +1,24 @@
+'''
+Dado uma lista de inteiros, retorne a quantidade de pares iguais.
+
+Exemplo:
+
+Entrada: nums = [1,2,3,1,1,3]
+Saída: 4
+Explicação: Existem 4 pares iguais (0,3), (0,4), (3,4), (2,5).
+
+https://leetcode.com/problems/number-of-good-pairs/
+'''
+
+class Solution:
+    def numIdenticalPairs(nums):
+        count_numbers = {}
+        pairs = 0
+        for num in nums:
+            if num in count_numbers:
+                pairs += count_numbers[num]
+                count_numbers[num] += 1
+            else:
+                count_numbers[num] = 1
+        return pairs
+    print(numIdenticalPairs([1,2,3,1,1,3]))
