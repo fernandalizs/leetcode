603b5a08c3a4ca548cb6d99105f56dc074cd617f
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-01-12 16:23:56 -0300

    add countDistinctInteger.js

---
+++ JavaScript/countDistinctIntegers.js
@@ -0,0 +1,26 @@
+/*
+Você recebe uma matriz de inteiros positivos.
+Você deve pegar cada número inteiro na matriz, inverter seus dígitos e adicioná-los ao final da matriz.
+Retorne o número de inteiros distintos na matriz final.
+
+Exemplo:
+
+Entrada: nums = [1,13,10,12,31]
+Saída: 6
+Explicação: Depois de incluir o reverso de cada número, a matriz resultante é [1,13,10,12,31,1,31,1,21,13].
+Observe que para o inteiro 10, após invertê-lo, ele se torna 01, que é o mesmo que 1.
+O número de inteiros distintos nesta matriz é 6 (sendo eles 1, 10, 12, 13, 21 e 31).
+
+https://leetcode.com/problems/count-number-of-distinct-integers-after-reverse-operations/description/
+*/
+
+const countDistinctIntegers = function(nums) {
+    let list = []
+    for (num of nums){
+        let revert = num.toString().split('').reverse().join('')
+        revertList.push(Number(rev))
+    }
+    let newList = nums.concat(list)
+    return [...new Set(newList)].length
+};
+console.log(countDistinctIntegers([1,13,10,12,31]))
