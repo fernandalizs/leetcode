45d8e1f29d1b2fcd53b9f7a3329da98ae5ac4f4c
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-01-13 18:07:37 -0300

    remove espaço sobrando

--- Easy/single_number.py
+++ Easy/single_number.py
@@ -12,7 +12,7 @@ https://leetcode.com/problems/single-number/
 class Solution(object):
     def single_number(nums):
         diff = nums[0]
-        for i in range (1, len(nums)): 
+        for i in range (1, len(nums)):
             diff ^= nums[i]
         return diff
     print(single_number([1,1,2,2,3]))
