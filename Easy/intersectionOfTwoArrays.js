938bd654a387ba6d606d860fa0c953cd939d430f
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-01-13 18:00:08 -0300

    ajusta descrição e variáveis

--- Easy/intersectionOfTwoArrays.js
+++ Easy/intersectionOfTwoArrays.js
@@ -1,7 +1,7 @@
 /* 
 Dado duas listas nums1 e nums2, retorne um array de sua interseção. Cada elemento no resultado deve ser único e você pode retornar o resultado em qualquer ordem.
 
-Exemplo 1:
+Exemplo:
 
 Entrada: nums1 = [1,2,2,1], nums2 = [2,2]
 Saída: [2]
@@ -12,13 +12,13 @@ https://leetcode.com/problems/intersection-of-two-arrays/description/
 const intersection = function(nums1, nums2) {
     let list1 = new Set(nums1)
     let list2 = new Set(nums2)
-    let list3 = []
-    for (let n of list1){
-        if (list2.has(n)){
-            list3.push(n)
+    let newList = []
+    for (let number of list1){
+        if (list2.has(number)){
+            newList.push(number)
         }
     }
-    return list3
+    return newList
 };
 let nums1 = [1,2,3,3]
 let nums2 = [2,3,4]
